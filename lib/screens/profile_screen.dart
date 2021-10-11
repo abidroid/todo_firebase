b7061dc83e4f53ce21d5194d215c17afb23658d3
abidroid <abidroid1@gmail.com>
2021-10-11 09:34:25 +0500

    profile screen design updated

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -50,135 +50,64 @@ class _ProfileScreenState extends State<ProfileScreen> {
         appBar: AppBar(
           title: Text('Profile'),
         ),
-        body: Container(
-          padding: EdgeInsets.all(8),
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                child: Stack(
-                  children: [
-                    ClipOval(
-                      child: Image.network(
-                        userModel == null
-                            ? placeholderImage
-                            : userModel!.profileImage == ''
-                                ? placeholderImage
-                                : userModel!.profileImage,
-                        width: 150,
-                        height: 150,
-                        fit: BoxFit.fill,
-                      ),
+        body: userModel == null
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+          children: [
+            Container(
+              width: 120,
+              height: 120,
+              child: Stack(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        userModel!.profileImage == '' ?
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnme6H9VJy3qLGvuHRIX8IK4jRpjo_xUWlTw&usqp=CAU'
+                            :
+                        userModel!.profileImage
+
+                    )
+                    ,
+                    radius: 80,
+                  ),
+                  Positioned(
+                    right: -10,
+                    bottom: -10,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.camera_alt),
                     ),
-                    Positioned(
-                      bottom: 5,
-                      right: 5,
-                      child: Container(
-                        width: 40,
-                        height: 40,
-                        padding: EdgeInsets.zero,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.red, width: 2),
-                            shape: BoxShape.circle),
-                        child: Center(
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.camera_alt,
-                              color: Colors.red,
-                            ),
-                            onPressed: () {
-
-                              // select and upload image to firebase storage
-
-
-                            },
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Card(
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Member Since: ${getHumanReadableDate(userModel!.dt)}'),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text('FullName ${userModel!.name}'),
                           ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Card(
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Joined'),
-                          Text(
-                            userModel != null
-                                ? getHumanReadableDate(userModel!.dt)
-                                : '',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          )
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {},
+                          ),
                         ],
-                      )
+                      ),
+                      Text('Email: ${userModel!.email}'),
                     ],
                   ),
                 ),
               ),
-              SizedBox(
-                height: 8,
-              ),
-              Card(
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Email'),
-                          Text(
-                            userModel != null ? '${userModel!.email}' : '',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Card(
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Full Name'),
-                          Text(
-                            userModel != null ? '${userModel!.name}' : '',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {
-                          print('Show Dialog');
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
